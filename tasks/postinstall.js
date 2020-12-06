f51851d50d5f612cbb7a14b3fc236f131d3dc794
Lawrence Cherone <lawrence@cherone.co.uk>
2020-12-06 23:32:12 +0000

    added postinstall message, update package.json

---
+++ tasks/postinstall.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+const msg = '\u001b[96m\u001b[1mLike\u001b[0m \u001b[93m\u001b[1mnode-postcodes.io?\u001b[0m\n\u001b[96mThen show your love by \u001b[93m🌟\u001b[0m \u001b[96mstaring the project at \u001b[32m\u001b[1mhttps://github.com/lcherone/node-postcodes.io\u001b[0m \u001b[96mthanks!\u001b[0m\n';
+console.log(msg)
